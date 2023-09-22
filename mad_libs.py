ce27a8123186f373a76933e2960cabb4424e70c6
Karla Kollontaj <carlavaleriabarnab@gmail.com>
2023-09-22 14:27:03 +0200

    mad_libs ok

---
+++ mad_libs.py
@@ -0,0 +1,20 @@
+#modos para concatenar cadenas de caracteres
+#supongamos que queremos crear una cadena que diga:
+#aprender a programar con _____
+
+# organizacion = "freeCodeCamp"
+
+# print("Aprende a programar con " + organizacion)
+# print("Aprende a programar con {}".format(organizacion))
+# print(f'Aprende a programar con {organizacion}')
+
+# Mad Libs
+
+adj = input("Adjetivo: ")
+verbo1 = input("Verbo: ")
+verbo2 = input("Verbo: ")
+sustantivo_plural = input("Sustantivo (plural): ")
+
+madlib = f"Programar es tan {adj}! Siempre me emociona porque me encanta {verbo1} problemas. Aprende a {verbo2} con freeCodeCamp y alcanza tus {sustantivo_plural}!"
+
+print(madlib)
